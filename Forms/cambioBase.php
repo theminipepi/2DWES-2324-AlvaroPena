721c36e80ab0ef6a58613ae10ef6e29eac8a2765
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2023-10-10 09:13:16 +0200

    Add files via upload

---
+++ Forms/cambioBase.php
@@ -0,0 +1,70 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+function calcular(){
+
+    $decimal=$Decimal=$_POST['NumDecimal'];
+
+    if($_POST['conversion']=='Binario'){
+        $binario=binario($decimal);
+    }
+    if($_POST['conversion']=='Octal'){
+        $octal=octal($decimal);
+    }
+    if($_POST['conversion']=='Hexadecimal'){
+        $hexadecimal=hexadecimal($decimal);
+    }
+    if($_POST['conversion']=='Todos Sistemas'){
+        $binario=binario($decimal);
+        $octal=octal($decimal);
+        $hexadecimal=hexadecimal($decimal);
+    }
+
+}
+}
+function binario($decimal){
+    $binario=decbin($decimal);
+    return $binario;
+}
+function octal($decimal){
+    $octal=octdec($decimal);
+    return $octal;
+}
+function hexadecimal($decimal){
+    $hexadecimal=dechex($decimal);
+    return $hexadecimal;
+}
+
+function pintar(){
+    if($_POST['conversion']=='Binario'){
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<td>$binario</td>";
+        echo "</tr>";
+        echo "</table>";
+    }
+    if($_POST['conversion']=='Octal'){
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<td>$octal</td>";
+        echo "</tr>";
+        echo "</table>";
+    }
+    if($_POST['conversion']=='Hexadecimal'){
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<td>$hexadecimal</td>";
+        echo "</tr>";
+        echo "</table>";
+    }
+    if($_POST['conversion']=='Todos Sistemas'){
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<td>$binario</td>";
+        echo "<td>$octal</td>";
+        echo "<td>$hexadecimal</td>";
+        echo "</tr>";
+        echo "</table>";
+        
+    }
+}
