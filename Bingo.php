b93a58cbc985eb7e66bdff5f111c0ec1fa698fac
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2023-10-24 11:01:42 +0200

    Add files via upload

---
+++ Bingo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bingo</title>
+</head>
+<body>
+<?php
+    $jugadores = [
+        "j1"=>$cartones1=array();
+        "j2"=>$cartones2=array();
+        "j3"=>$cartones3=array();
+    ];
+
+    foreach($jugadores as $j){
+        for()
+    }
+    var_dump($jugadores);
+
+?>
+</body>
+</html>
