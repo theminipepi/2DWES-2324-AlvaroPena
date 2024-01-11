a43d19820c57682768b6d089f3da951d2d6f4a45
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2024-01-11 09:24:50 +0100

    Add files via upload

---
+++ Base-Datos/ComprasWeb/compras.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+     
+    
+    <a href="./compraProductos.php" tittle="compras">
+    <img src='./Imagenes/Carrito.png' alt="compras">
+    </a>
+    <br>
+    <a href="./consultaCompras.php">Consulta de Compras</a>
+
+    </form>
+</body>
+</html>
