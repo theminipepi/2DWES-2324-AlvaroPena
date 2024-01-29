d09466818f80f66cac31a30d72d49c3ecc0fb829
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2024-01-29 11:02:25 +0100

    Add files via upload

---
+++ Base-Datos/WebPedidos/pe_topprod.php
@@ -0,0 +1,39 @@
+<?php
+include "funciones.php";
+$conn = conexion();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <label for="fechaIni">Desde:
+    <input type="date" name="fechaIni"></input>
+    <br><br>
+    <label for="fechaFin">Hasta:
+    <input type="date" name="fechaFin"></input>
+    <input type="submit" value="enviar">
+</form>
+</body>
+</html>
+<?php
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+    try {
+       
+        $fecha_ini = $_POST['fechaIni'];
+        $fecha_fin = $_POST['fechaFin'];
+        $nombre_producto = entrefechas($conn,$fecha_ini,$fecha_fin);
+        var_dump($nombre_producto);
+
+        }
+    catch(PDOException $e)
+        {
+        echo "Error: " . $e->getMessage();
+        }
+    $conn = null;
+    }
+?>
