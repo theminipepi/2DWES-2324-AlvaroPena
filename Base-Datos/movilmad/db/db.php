cd73b632c3297e0e6120f390e61d172750f6c93e
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2024-02-12 09:50:18 +0100

    Add files via upload

---
+++ Base-Datos/movilmad/db/db.php
@@ -0,0 +1,14 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "rootroot";
+	$database = "movilmad";
+
+	try {
+		$conexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password); 	 	 	 	 	 	
+		$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	 	 	 	 	
+	} catch (PDOException $ex) {
+		echo $ex->getMessage(); 	 	 	 	 	 	
+	}
+?>
