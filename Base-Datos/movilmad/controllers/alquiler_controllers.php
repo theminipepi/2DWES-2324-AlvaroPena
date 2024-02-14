aeae12d9b42311d42af46ed8a76e31f339ee9eb4
Álvaro Peña <128598882+theminipepi@users.noreply.github.com>
2024-02-14 13:42:43 +0100

    Add files via upload

---
+++ Base-Datos/movilmad/controllers/alquiler_controllers.php
@@ -0,0 +1,27 @@
+<?php
+require_once("../models/realizaralquiler_models.php");
+
+    if (!isset($_SESSION['carrito'])) {
+        $_SESSION['carrito'] = array();
+    }
+
+    if (isset($_POST['agregar'])) {
+        if(count($_SESSION['carrito']) < 3){
+            array_push( $_SESSION['carrito'], $_POST['vehiculos']);
+        }
+            
+    }
+
+    if (isset($_POST['vaciar'])) {
+        unset($_SESSION['carrito']);
+    }
+
+    if(isset($_POST['alquilar'])){
+       foreach($_SESSION['carrito'] as $x){
+        realizarAlquiler_models($_SESSION['idcliente'], $x);
+        header("Location: ../views/movwelcome.php");
+        unset($_SESSION['carrito']);
+       }
+    }
+    
+?>
